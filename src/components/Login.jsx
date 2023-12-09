ccfed5e8e3cd1adead9555b0b2bf31e37de396dd
jhakes05 <jake.atillo.b@gmail.edu.ph>
2023-12-09 21:27:43 +0800

    aylabyu

---
+++ src/components/Login.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from 'react';
+import Main from "./Main"
+import { 
+    Link } from 'react-router-dom';
+const Login = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleLogin = (event) => {
+        event.preventDefault();
+
+        // Temporary admin credentials (to be deleted in development)
+        const tempAdminUsername = 'admin';
+        const tempAdminPassword = 'admin123';
+
+        if (username === tempAdminUsername && password === tempAdminPassword) {
+            window.location.href = <Link to='/main'/>; // Replace with React Router navigation
+        } else {
+            alert('Invalid username or password. Please try again.');
+        }
+    };
+
+    const redirectToCreateAccount = () => {
+        alert('Redirecting to create account page. Implement your logic here.');
+        // Add your logic to redirect to the create account page.
+    };
+
+    return (
+        <div className="login-page">
+            <div className="login-container">
+                <h2>Login to Certification Management System</h2>
+                <form onSubmit={handleLogin}>
+                    <label htmlFor="username">Username:</label>
+                    <input
+                        type="text"
+                        id="username"
+                        name="username"
+                        value={username}
+                        onChange={(e) => setUsername(e.target.value)}
+                        required
+                    />
+
+                    <label htmlFor="password">Password:</label>
+                    <input
+                        type="password"
+                        id="password"
+                        name="password"
+                        value={password}
+                        onChange={(e) => setPassword(e.target.value)}
+                        required
+                    />
+
+                    <button type="submit">Login</button>
+                </form>
+                <Link to='/'>
+                <button className="create-account-btn" >
+                   yes
+                </button>
+                </Link>
+                {/* Create Account Button */}
+                <button className="create-account-btn" onClick={redirectToCreateAccount}>
+                    Create Account
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/components/Main.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+const Dashboard = () => {
+    const openCreateForm = (functionName) => {
+        alert(`You clicked on "${functionName}" button. Implement your logic here.`);
+        // Add your logic to open a create form or perform other actions.
+    };
+
+    return (
+        <div>
+            <header>
+                <h1>Certification Management System</h1>
+                <a href="index.html">Logout</a>
+            </header>
+
+            <section className="dashboard-content">
+                <div className="dashboard-section" id="manage-certifications">
+                    <h2>Manage Certifications</h2>
+                    {/* Your content for managing certifications goes here */}
+                    <p>This is where you manage certifications.</p>
+                    {/* Button for "Manage Certifications" */}
+                    <button onClick={() => openCreateForm('Create Certification')}>Create Certification</button>
+                </div>
+
+                <div className="dashboard-section" id="view-certificates">
+                    <h2>View Certificates</h2>
+                    {/* Your content for viewing certificates goes here */}
+                    <p>This is where you view certificates.</p>
+                    {/* Button for "View Certificates" */}
+                    <button onClick={() => openCreateForm('Create Certificate')}>Create Certificate</button>
+                </div>
+
+                <div className="dashboard-section" id="user-management">
+                    <h2>User Management</h2>
+                    {/* Your content for user management goes here */}
+                    <p>This is where you manage users.</p>
+                    {/* Button for "User Management" */}
+                    <button onClick={() => openCreateForm('Create User')}>Create User</button>
+                </div>
+            </section>
+        </div>
+    );
+};
+
+export default Dashboard;
